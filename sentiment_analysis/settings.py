392c00a6afb369a8c66d62c4b6a18ac3e1b36f56
Pramod <57028365+Pramod025@users.noreply.github.com>
2024-02-04 21:59:05 +0530

    Update settings.py

--- sentiment_analysis/settings.py
+++ sentiment_analysis/settings.py
@@ -21,10 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k_9kj%+=ep(*kz671g0fqoempgny6r9ooccq!wcn3jn%-lc3hp'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-k_9kj%+=ep(*kz671g0fqoempgny6r9ooccq!wcn3jn%-lc3hp')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+
 
 ALLOWED_HOSTS = []
 
